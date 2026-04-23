47886fced3e480fe378e6153471b0bf4428bc9e4
nam3mc <moralesdreiser5gmail.com>
2026-04-23 07:51:39 -0500

    studying the entities structures

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Changes')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('properties')
   getHello(): string {
-    return this.appService.getHello();
+    let a = 100
+    return a.toString()
   }
 }
