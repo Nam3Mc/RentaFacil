d8c94788b757f1fa11910e538bff5f53f47a7132
nam3mc <moralesdreiser5gmail.com>
2026-04-24 14:17:39 -0500

    prisma configured missing database creation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaClient } from './generated/prisma/client.js';
+import { PrismaPg } from "@prisma/adapter-pg"; // or the adapter for your database
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
