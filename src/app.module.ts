27993ee9fcaca9dd9c3f94b7a7796c3ce8c3ccb9
nam3mc <moralesdreiser5gmail.com>
2026-04-24 23:22:02 -0500

    prisma and data base working, first module users working, pending work in all the business logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { UserModule } from './user/user.module.js';
+import { UserController } from './user/user.controller.js';
+import { UserService } from './user/user.service.js';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { PrismaClient } from './generated/prisma/client.js';
-import { PrismaPg } from "@prisma/adapter-pg"; // or the adapter for your database
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('RentaFacil')
-    .setDescription('An interactive renting by month app taht connect onwers and renters in the safest way.')
+    .setDescription('An interactive renting by month app that connects owners and renters safely.')
     .setVersion('1.0')
-    .build()
+    .build();
 
-  const document = SwaggerModule.createDocument(app, config)
-
-  SwaggerModule.setup('API', app, document)
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('API', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
